22ccb07e0901e85786142c09665928324d0ad448
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-08-11 23:25:45 +0500

    Add files via upload

---
+++ Java Script /Assignment-02/app.js
@@ -0,0 +1,36 @@
+let a = 3
+let b = 5
+// let a = input("enter a number")
+let c = a+b
+console.log("sum of" , a, "and", b, "=", c)
+
+
+// table of 4
+let num1 = 4 ;
+for (let i=1; i<11; i++){
+    table = num1*i;
+    console.log(num1 ,"*", i,"=", table)
+}
+
+// temperature converter frenhite to centigrate
+let f = 212
+let celcious = (f-32)*5/9 
+console.log(celcious)
+
+//  temperature converter  centigrate to frenhite
+let cel = 32
+let fren = (cel * (9/5))+32
+console.log(fren)
+
+
+// Age Calculate
+let currentYear = 2025
+let BirthYear = 2006
+let age = currentYear - BirthYear
+console.log(age)
+
+
+//   let a =10
+//   let b = 20
+//   let c=a+b
+//   console.log(c);
